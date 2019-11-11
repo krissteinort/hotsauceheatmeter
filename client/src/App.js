de58f385ebdfadb3eb5d7dbc504cc83d742f766f
Kris-Steinort <kbsteinort@gmail.com>
2019-11-11 17:28:32 -0500

    adds app structure and images

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ScovilleMeter from './components/ScovilleMeter';
+
+function App() {
+  return (
+  <div>
+    <ScovilleMeter scovilles={5000}/>
+  </div>
+  );
+}
+
+export default App;
